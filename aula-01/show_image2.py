664d3a0525ceb9e165ebb93bce51cb4dc56d9b15
AllanCapistrano <allan.capistrano3@gmail.com>
2023-03-10 13:52:41 -0300

    Feat: move files

--- aula-01/show_image2.py
+++ aula-01/show_image2.py
@@ -1,7 +1,7 @@
 import cv2
 from pathlib import Path
 
-image_path = Path('aula-01/images/len_std.png')
+image_path = Path('images/len_std.png')
 image      = cv2.imread(str(image_path))
 
 cv2.imshow('Loaded Image', image)
