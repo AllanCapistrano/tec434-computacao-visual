35e0ca9c48ad2ce66e2c32bfa578d6f410809b29
AllanCapistrano <allan.capistrano3@gmail.com>
2023-03-17 14:27:04 -0300

    Style: format code

--- aula-02/negative.py
+++ aula-02/negative.py
@@ -5,7 +5,7 @@ image_path = Path('images/len_std.png')
 image      = cv2.imread(str(image_path))
 
 negative_image = ~image
-back_image = ~negative_image
+back_image     = ~negative_image
 
 cv2.imshow('Loaded Image', image)
 cv2.imshow('Negative Image', negative_image)
