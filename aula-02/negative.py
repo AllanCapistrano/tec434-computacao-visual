a5cba1230632f3907d35dcd0b444cd7a4f702da2
AllanCapistrano <allan.capistrano3@gmail.com>
2023-03-17 14:26:34 -0300

    Feat: add aula-02 file

---
+++ aula-02/negative.py
@@ -0,0 +1,14 @@
+import cv2
+from pathlib import Path
+
+image_path = Path('images/len_std.png')
+image      = cv2.imread(str(image_path))
+
+negative_image = ~image
+back_image = ~negative_image
+
+cv2.imshow('Loaded Image', image)
+cv2.imshow('Negative Image', negative_image)
+cv2.imshow('Back Image', back_image)
+
+cv2.waitKey(0)
