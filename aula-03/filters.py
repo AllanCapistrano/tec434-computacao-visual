9acca6c24a5a67913ffaa740674b4786325c3a2f
AllanCapistrano <allan.capistrano3@gmail.com>
2023-03-24 14:18:48 -0300

    Feat: add high-pass and low-pass filters

---
+++ aula-03/filters.py
@@ -0,0 +1,30 @@
+import cv2
+from numpy import array
+from pathlib import Path
+
+image_path = Path('images/len_gray.png')
+image      = cv2.imread(str(image_path))
+
+cv2.imshow('Loaded Image', image)
+
+low_pass = cv2.blur(image,(9,9))
+
+cv2.imshow('Low Pass Filter', low_pass)
+
+mask_4 = array([[0, -1, 0],
+                [-1,  4, -1],
+                [0, -1, 0]])
+
+mask_8 = array([[-1, -1, -1],
+                [-1,  8, -1],
+                [-1, -1, -1]])
+
+high_pass_4 = cv2.filter2D(low_pass, ddepth=None, kernel=mask_4)
+high_pass_8 = cv2.filter2D(low_pass, ddepth=None, kernel=mask_8)
+
+cv2.imshow('High Pass Filter 4', high_pass_4)
+
+cv2.imshow('High Pass Filter 8', high_pass_8)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
